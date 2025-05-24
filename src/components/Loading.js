961691c947a8f13963e3a8f29052cef8007e6c6d
absabur <absabur929@gmail.com>
2025-05-25 01:00:24 +0600

    profile image upload solve

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,18 +5,17 @@ import "@/css/loading.css";
 
 const Loading = ({ manualLoading = false }) => {
   const isLoading = useSelector((state) => state.isLoading);
-  
+
   return (
     <>
-      {isLoading ||
-        (manualLoading && (
-          <div className="loading-overlay">
-            <div className="loader">
-              <div className="spinner" />
-              <p>Loading...</p>
-            </div>
+      {(isLoading || manualLoading) && (
+        <div className="loading-overlay">
+          <div className="loader">
+            <div className="spinner" />
+            <p>Loading...</p>
           </div>
-        ))}
+        </div>
+      )}
     </>
   );
 };
